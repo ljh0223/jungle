d8b40cbbb8552dc4f4ec2dc563863de81b41fd98
이재혁 <cncnvmvm@naver.com>
2026-03-09 11:38:43 +0900

    백준 첫 문제풀이, 사용법 알아가는중, 최댓값과 그 위치 (close #17)

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,23 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+
+#테스트용
+test_data=[3,29,38,12,57,74,40,85,61]
+
+number = []
+for num in test_data:
+    number.append(num)
+
+m=max(number)
+print (m, number.index(m)+1)
+
+
+
+#제출용
+number = []
+for _ in range(9):
+    number.append(int(input()))
+
+m=max(number)
+print (m, number.index(m)+1)
