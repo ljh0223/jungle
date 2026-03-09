bb930b13ae4b4d9316b160bf5802337aa0efc83d
이재혁 <cncnvmvm@naver.com>
2026-03-09 14:14:11 +0900

    백준-소수 찾기, 백준 적응중 (close #22)

--- week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
+++ week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
@@ -1,2 +1,20 @@
 # 정수론 - 소수 찾기 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/1978
+n=input()
+data=list(map(int, input().split()))
+
+def sol():
+    prime=[]
+    for i in data:
+        if i<2:
+            continue
+        elif i==2:
+            prime.append(i)
+        else:
+            for j in range(2,int(i**0.5)+1):
+                if i%j==0:
+                   break
+            else:
+                prime.append(i)
+
+    return len(prime)
