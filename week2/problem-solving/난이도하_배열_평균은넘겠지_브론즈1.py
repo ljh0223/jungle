1e693a53651b9669c9af01b97808bd637239e9f4
이재혁 <cncnvmvm@naver.com>
2026-03-10 01:28:38 +0900

    평균은 넘겠지 (close #18)

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,28 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+
+import sys
+# 로컬 PC(VS Code) 환경에서만 파일을 읽어오도록 설정
+# 백준 서버에 제출할 때는 이 아랫부분은 지우거나 주석 처리해야 합니다.
+sys.stdin = open("4344.txt", "r")
+
+def solve():
+    c = int(sys.stdin.readline()) #테스트 케이스 개수
+    avg_list=[]
+    n_list=[]
+    for _ in range(c):
+        data = list(map(int, sys.stdin.readline().split()))
+        n = data[0]
+        n_list.append(n)         # 학생 수
+        scores = data[1:]   # 점수 리스트
+        avg=sum(scores)/n
+        avg_list.append(avg)
+        count=0
+        for s in scores:
+            if s > avg:
+                count+=1
+    
+        rate=(count/n)*100
+        print(f"{rate:.3f}%")
+
+solve()
