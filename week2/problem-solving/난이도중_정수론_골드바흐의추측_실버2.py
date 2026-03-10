6c02c5ccd728ecfe11c6a33f8cfda5375d06ddb7
이재혁 <cncnvmvm@naver.com>
2026-03-10 17:33:52 +0900

    백준 골드바흐 추측, 차이가 작은 수 찾는 법 확인 (close #28)

--- week2/problem-solving/난이도중_정수론_골드바흐의추측_실버2.py
+++ week2/problem-solving/난이도중_정수론_골드바흐의추측_실버2.py
@@ -1,2 +1,33 @@
 # 정수론 - 골드바흐의 추측 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/9020
+def sol():
+    def is_prime(p):
+        if p<=1:
+            return False
+        if p==2:
+            return True
+        if p>=3:
+            for i in range(2,int(p**(1/2))+1):
+                if p%i==0:
+                    break
+            else:
+                return True
+    
+    prime=[]
+    for i in range(100):
+        if is_prime(i)==True:
+            prime.append(i)
+    
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        a=n//2
+        b=n//2
+
+        while True:
+            if is_prime(a) and is_prime(b):
+                print(f"{a} {b}")
+                break
+            a-=1
+            b+=1
+sol()
