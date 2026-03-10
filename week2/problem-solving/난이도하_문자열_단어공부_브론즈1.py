17a13b5a336b5baf9aabbe2110a1b02d2155e6b5
이재혁 <cncnvmvm@naver.com>
2026-03-10 11:17:44 +0900

    백준 단어공부, 딕셔너리를 활용해보자 (close #20)

--- week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
+++ week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
@@ -1,2 +1,26 @@
 # 문자열 - 단어 공부 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/1157
+def sol():
+    word=input().upper()
+    counts={}
+    for char in word:
+        if char in counts:
+            counts[char]+=1
+        else:
+            counts[char]=1
+
+    max_count=0
+    for val in counts.values():
+        if val > max_count:
+            max_count=val
+    result=[]
+    for key, val in counts.items():
+        if val == max_count:
+            result.append(key)
+    if len(result)>1:
+        print("?")
+    else:
+        print(result[0])
+
+if __name__ == "__main__":
+    sol()
--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,13 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+S="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ$%*+-./:"
+T=int(input())
+for _ in range(T):
+    R,t=input().split()
+    R=int(R)
+    result=""
+    for alpha in t:
+        ind=S.index(alpha)
+        result+=R*S[ind]
+    print(result)
+
