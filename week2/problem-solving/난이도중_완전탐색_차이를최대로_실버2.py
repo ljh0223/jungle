4cc36d3919693b9f66213e3ebceddaf20b339f8c
이재혁 <cncnvmvm@naver.com>
2026-03-12 20:28:19 +0900

    백준 차이를 최대로, permutations (close #24)

--- week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
+++ week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
@@ -1,2 +1,21 @@
 # 완전탐색 - 차이를 최대로 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/10819
+
+from itertools import permutations
+
+def sol():
+    n=int(input())
+    result=[]
+    nums=list(map(int, input().split()))
+    if n==0: return 0
+
+    for p in permutations(nums):
+        b=0
+        for i in range(len(p)-1):
+            a=p[i]-p[i+1]
+            b+=abs(a)
+        result.append(b)
+
+    print(max(result))
+
+sol()
