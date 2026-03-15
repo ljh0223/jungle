670fa234c1a2a86a6fa41efd1d0e2a3076d93157
이재혁 <cncnvmvm@naver.com>
2026-03-15 20:49:40 +0900

    백준9933 해시테이블 (close #57)

--- week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,2 +1,28 @@
 # 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/9933
+N=int(input())
+
+def sol():
+    pw_list=[]
+    result=[]
+    for _ in range(N):
+        pw_list.append(input())
+    for pw in pw_list:
+        if pw[::-1] in pw_list:
+            if not result:
+                result.append(pw)
+
+    return print(len(result[0]), result[0][len(result[0])//2] )
+
+
+
+def sol2():
+    pw_set=set()
+    for _ in range(N):
+        pw_set.add(input())
+    for pw in pw_set:
+        if pw[::-1] in pw_set:
+            print(len(pw), pw[(len(pw))//2])
+            break
+
+sol2()
