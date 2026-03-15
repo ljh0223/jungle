4c1fa5a96230e6b219dd2dd63e68abe442f829ff
이재혁 <cncnvmvm@naver.com>
2026-03-15 22:53:58 +0900

    deque로 큐 구현 (close #55)

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,14 @@
 # 큐 - 카드2 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/2164
+from collections import deque
+
+def sol():
+
+    n=int(input())
+    queue=deque(i+1 for i in range(n))
+    while len(queue) != 1:
+        queue.popleft()
+        queue.append(queue.popleft())
+    print(queue[0])
+
+sol()
