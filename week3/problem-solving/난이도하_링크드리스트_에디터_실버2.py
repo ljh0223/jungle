239436c931b3c682d7755c53ea5f50119ec72218
이재혁 <cncnvmvm@naver.com>
2026-03-16 22:57:33 +0900

    백준 1406 연결리스트 에디트 이해중 (close #56)

--- week3/problem-solving/난이도하_링크드리스트_에디터_실버2.py
+++ week3/problem-solving/난이도하_링크드리스트_에디터_실버2.py
@@ -1,5 +1,6 @@
 # 링크드리스트 - 에디터 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1406
+'''''''''
 def sol():
 
     left=list(input().strip())
@@ -18,3 +19,74 @@ def sol():
             left.append(a[1])
     return print("".join(left+right[::-1]))
 sol()
+'''
+
+import sys
+
+input = sys.stdin.readline
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+text = input().rstrip()
+m = int(input())
+
+head = Node(None)
+tail = Node(None)
+head.next = tail
+tail.prev = head
+
+cursor = tail
+
+for ch in text:
+    new_node = Node(ch)
+    left = cursor.prev
+
+    left.next = new_node
+    new_node.prev = left
+
+    new_node.next = cursor
+    cursor.prev = new_node
+
+for _ in range(m):
+    command = input().split()
+
+    if command[0] == 'L':
+        if cursor.prev != head:
+            cursor = cursor.prev
+
+    elif command[0] == 'D':
+        if cursor != tail:
+            cursor = cursor.next
+
+    elif command[0] == 'B':
+        if cursor.prev != head:
+            target = cursor.prev
+            left = target.prev
+
+            left.next = cursor
+            cursor.prev = left
+
+    else:
+        new_node = Node(command[1])
+        left = cursor.prev
+
+        left.next = new_node
+        new_node.prev = left
+
+        new_node.next = cursor
+        cursor.prev = new_node
+
+result = []
+current = head.next
+
+while current != tail:
+    result.append(current.value)
+    current = current.next
+
+print(''.join(result))
