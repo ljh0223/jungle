f5d29f6c0380d0a55fb4b65c9c4a0ff67771ec0b
이재혁 <cncnvmvm@naver.com>
2026-03-16 15:03:33 +0900

    백준2630 색종이 분할정복 (close #53)

--- week3/problem-solving/난이도하_분할정복_색종이만들기_실버2.py
+++ week3/problem-solving/난이도하_분할정복_색종이만들기_실버2.py
@@ -1,2 +1,27 @@
 # 분할정복 - 색종이 만들기 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/2630
+def sol():
+    s=int(input())
+    nums=[list(map(int,input().split())) for _ in range(s)]
+    white_count=0
+    blue_count=0
+
+    def cut(x,y,s):
+        nonlocal white_count, blue_count
+        sum_num=sum(sum(row[y:y+s]) for row in nums[x:x+s])
+        if sum_num==0:
+            white_count+=1
+            return
+        elif sum_num==s**2:
+            blue_count+=1
+            return     
+        half=s//2
+        cut(x,y,half)
+        cut(x,y+half,half)
+        cut(x+half,y,half)
+        cut(x+half,y+half,half)
+
+    cut(0,0,s)
+    return print(white_count), print(blue_count)
+
+sol()
