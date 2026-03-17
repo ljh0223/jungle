50f4d81c2d495aa9d3472046f79f55f4f62b66cb
이재혁 <cncnvmvm@naver.com>
2026-03-17 16:20:05 +0900

    백준2470 두 용액 (close #58)

--- week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
+++ week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
@@ -1,2 +1,31 @@
 # 이분탐색 - 두 용액 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2470
+n=int(input())
+liquid=sorted(map(int,input().split()))
+mixed=[]
+def sol():
+    left=0
+    right=len(liquid)-1
+    min_mix=float('inf')
+    answer_left=0
+    answer_right=0
+
+    while left<right:
+        mix=liquid[left]+liquid[right]
+        if mix==0:
+            return print(liquid[left],liquid[right])
+        elif mix<0:
+            if abs(mix)<min_mix:
+                min_mix=abs(mix)
+                answer_left=liquid[left]
+                answer_right=liquid[right]
+            left+=1
+        else:
+            if abs(mix)<min_mix:
+                min_mix=abs(mix)
+                answer_left=liquid[left]
+                answer_right=liquid[right]
+            right-=1
+    return print(answer_left, answer_right)
+
+sol()
