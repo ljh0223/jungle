20910d532f16f2d01aaec3a44e4188b183dbec78
이재혁 <cncnvmvm@naver.com>
2026-03-21 18:04:36 +0900

    백준 14244 트리만들기

--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,9 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+n,m=map(int,input().split())
+
+for i in range(n-m):
+    print(i,i+1)
+
+for j in range(n-m,n-1):
+    print(n-m,j+1)
