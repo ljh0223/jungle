2c1da2f1c5c47bd7a5ad403409c7d3723ba5171a
이재혁 <cncnvmvm@naver.com>
2026-03-23 11:16:45 +0900

    백준16173 BFS (close#86)

--- week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,2 +1,10 @@
 # 트리 - 상근이의 여행 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/9372
+t=int(input())
+for _ in range(t):
+    n,m=map(int,input().split())
+    
+    for _ in range(m):
+        a,b=map(int,input().split())
+    
+    print(n-1)
