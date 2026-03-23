dc01f76ba6d0a4ef3d7abc7ff952e36c3415c178
이재혁 <cncnvmvm@naver.com>
2026-03-23 21:57:15 +0900

    백준 2294 동전2 이해 부족 (close #94)

--- week4/problem-solving/난이도중_BFS_동전2_골드5.py
+++ week4/problem-solving/난이도중_BFS_동전2_골드5.py
@@ -1,2 +1,17 @@
 # BFS - 동전 2 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2294
+    
+n,k=map(int,input().split())
+coins=[int(input()) for _ in range(n)]
+
+dp=[100001]*(k+1)
+dp[0]=0
+
+for coin in coins:
+    for i in range(coin,k+1):
+        dp[i]=min(dp[i], dp[i-coin]+1)
+
+if dp[k]==100001:
+    print(-1)
+else:
+    print(dp[k])
--- week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
+++ week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
@@ -1,2 +1,26 @@
 # 트리 - 이진 검색 트리 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/5639
+nums=[]
+while True:
+    try:
+        nums.append(int(input()))
+    except:
+        break
+
+def recursion(start,end):
+    if start>end:
+        return
+    
+    root=nums[start]
+    mid=end+1
+
+    for i in range(start+1,end+1):
+        if nums[i]>root:
+            mid=i
+            break
+
+    recursion(start+1,mid-1)
+    recursion(mid,end)
+    print(root)
+
+recursion(0,len(nums)-1)
