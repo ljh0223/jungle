8393bde24713df56a6909d8bbbddf0d553822311
이재혁 <cncnvmvm@naver.com>
2026-03-23 15:41:46 +0900

    백준 2606 바이러스 (close#87)

--- week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
@@ -1,2 +1,27 @@
 # 그래프, DFS, BFS - 바이러스 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/2606
+from collections import deque
+n=int(input())
+m=int(input())
+
+graph=[[] for i in range(n)]
+for _ in range(m):
+    x,y=map(int,input().split())
+    graph[x-1].append(y-1)
+    graph[y-1].append(x-1)
+
+visited=[False]*n
+visited[0]=True
+
+queue=deque([0])
+
+while queue:
+    now=queue.popleft()
+
+
+    for nxt in graph[now]:
+        if visited[nxt]==False:
+            visited[nxt]=True
+            queue.append(nxt)
+
+print(visited.count(True)-1)
--- week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
@@ -1,2 +1,54 @@
 # 그래프, DFS, BFS - 점프왕 쩰리 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/16173
+"""
+from collections import deque
+
+n=int(input())
+board=[list(map(int,input().split())) for _ in range(n)]
+visited=[[False]*n for _ in range(n)]
+
+queue=deque([(0,0)])
+visited[0][0]=True
+
+while queue:
+    x,y=queue.popleft()
+    if x==n-1 and y==n-1:
+        print("HaruHaru")
+        break
+    
+    jump=board[x][y]
+    nx,ny=x+jump,y
+    if nx<n and not visited[nx][ny]:
+        visited[nx][ny]=True
+        queue.append((nx,ny))
+    
+    nx,ny=x,y+jump
+    if ny<n and not visited[nx][ny]:
+        visited[nx][ny]=True
+        queue.append((nx,ny))
+
+else:
+    print("Hing")
+"""
+
+n=int(input())
+board=[list(map(int,input().split())) for _ in range(n)]
+visited=[[False]*n for _ in range(n)]
+visited[0][0]=True
+def recursion(x,y):
+    jump=board[x][y]
+    if x==n-1 and y==n-1:
+        visited[n-1][n-1]=True
+        return print("HaruHaru")
+    
+    if x+jump<n:
+        recursion(x+jump,y)
+        visited[x+jump][y]=True
+    if y+jump<n:
+        recursion(x,y+jump)
+        visited[x][y+jump]
+
+
+recursion(0,0)
+if visited[n-1][n-1]==False:
+    print("Hing")
