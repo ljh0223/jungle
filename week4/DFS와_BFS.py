b97284721168395cd893b3f8c6ddc2449df721d4
이재혁 <cncnvmvm@naver.com>
2026-03-24 19:52:44 +0900

    백준 1260 bfs,dfs 구현 완료 (close #90)

--- week4/DFS와_BFS.py
+++ week4/DFS와_BFS.py
@@ -1,9 +1,13 @@
+from collections import deque
+
 v,e,start=map(int,input().split())
 graph=[[]for _ in range(v+1)]
 for _ in range(e):
     a,b=map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
+for i in range(1,v+1):
+    graph[i].sort()
 visited=set()
 order=[]
 
@@ -14,6 +18,25 @@ def dfs(start):
     for nxt in graph[start]:
         if nxt not in visited:
             dfs(nxt)
-            print(*order)
-    
+            
 dfs(start)
+print(*order)
+
+def bfs():
+    queue=deque([start])
+    visited_que=set()
+    visited_que.add(start)
+    result=[start]
+
+    while queue:
+        now=queue.popleft()
+
+        for nxt in graph[now]:
+            if nxt not in visited_que:
+                visited_que.add(nxt)
+                result.append(nxt)
+                queue.append(nxt)
+
+    print(*result)
+
+bfs()
--- week4/problem-solving/난이도중_그래프DFSBFS_DFS와BFS_실버2.py
+++ week4/problem-solving/난이도중_그래프DFSBFS_DFS와BFS_실버2.py
@@ -1,2 +1,46 @@
 # 그래프, DFS, BFS - DFS와 BFS (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1260
+
+
+from collections import deque
+
+v,e,start=map(int,input().split())
+graph=[[]for _ in range(v+1)]
+for _ in range(e):
+    a,b=map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+for i in range(1,v+1):
+    graph[i].sort()
+visited=set()
+order=[]
+
+def dfs(start):
+    visited.add(start)
+    order.append(start)
+    
+    for nxt in graph[start]:
+        if nxt not in visited:
+            dfs(nxt)
+            
+dfs(start)
+print(*order)
+
+def bfs():
+    queue=deque([start])
+    visited_que=set()
+    visited_que.add(start)
+    result=[start]
+
+    while queue:
+        now=queue.popleft()
+
+        for nxt in graph[now]:
+            if nxt not in visited_que:
+                visited_que.add(nxt)
+                result.append(nxt)
+                queue.append(nxt)
+
+    print(*result)
+
+bfs()
