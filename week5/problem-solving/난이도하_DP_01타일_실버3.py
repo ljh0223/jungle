1f641b7765fb3dee33d94e0f1a72b113f4fd4415
이재혁 <cncnvmvm@naver.com>
2026-03-27 17:26:32 +0900

    백준 1904 타일 dp

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,2 +1,13 @@
 # DP - 01타일 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/1904
+n=int(input())
+def f(n):
+    if n <=2:
+        return n
+    a,b=1,2
+    for _ in range(3,n+1):
+        a,b=b,(a+b)%15746
+
+    return b
+
+print(f(n))
