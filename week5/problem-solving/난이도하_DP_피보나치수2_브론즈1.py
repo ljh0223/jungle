dab9cafe21c6ad448046ddbf223d27db2bf7f40a
이재혁 <cncnvmvm@naver.com>
2026-03-27 17:31:25 +0900

    백준2748 피보나치 dp

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,13 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+n=int(input())
+
+def f(n):
+    if n<=1:
+        return n
+    a,b=0,1
+    for _ in range(2,n+1):
+        a,b=b,a+b
+    return b
+
+print(f(n))
