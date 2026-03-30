25f1f40afe1b5f04b616ac98e1d96caf0d7a064c
이재혁 <cncnvmvm@naver.com>
2026-03-30 13:43:37 +0900

    백준 1946 신입사원 그리디

--- week5/problem-solving/난이도중_그리디_신입사원_실버1.py
+++ week5/problem-solving/난이도중_그리디_신입사원_실버1.py
@@ -1,2 +1,17 @@
 # 그리디 - 신입 사원 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1946
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    result=[]
+    scores=[tuple(map(int,input().split())) for _ in range(n)]
+
+    scores.sort()
+    current=scores[0][1]
+    count=1
+    
+    for score in scores[1:]:
+        if score[1]<current:
+            current=score[1]
+            count+=1
+    print(count)
--- week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
+++ week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
@@ -1,2 +1,20 @@
 # 그리디 - 회의실 배정 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1931
+import sys
+input = sys.stdin.readline
+
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    result=[]
+    scores=[tuple(map(int,input().split())) for _ in range(n)]
+
+    scores.sort()
+    current=scores[0][1]
+    count=1
+    
+    for score in scores[1:]:
+        if score[1]<current:
+            current=score[1]
+            count+=1
+    print(count)
