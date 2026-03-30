2a89449bdc4b153da5aef118b4540d0f69b76028
이재혁 <cncnvmvm@naver.com>
2026-03-30 11:22:50 +0900

    백준 1541 동전, 인풋값 처리 방법

--- week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -1,2 +1,8 @@
 # 그리디 - 잃어버린 괄호 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1541
+sol=input().split("-")
+answer=sum(map(int,sol[0].split('+')))
+for s in sol[1:]:
+    answer-=sum(map(int,s.split('+')))
+
+print(answer)
