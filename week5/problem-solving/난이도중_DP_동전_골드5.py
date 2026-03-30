ac80b8d776ca55522197044d60862e58ac5091db
이재혁 <cncnvmvm@naver.com>
2026-03-30 19:29:53 +0900

    백준9084 dp 동전 해결

--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,16 @@
 # DP - 동전 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9084
+
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    coins=list(map(int,input().split()))
+    k=int(input())
+
+    dp=[0]*(k+1)
+    dp[0]=1
+    for coin in coins:
+        for i in range(coin,k+1):
+            dp[i]+=dp[i-coin]
+
+    print(dp[k])
--- week5/동전.py
+++ week5/동전.py
@@ -1,11 +1,13 @@
-T=int(input())
-for _ in range(T):
+t=int(input())
+for _ in range(t):
     n=int(input())
     coins=list(map(int,input().split()))
-    total=int(input())
-    dp=[0]*(total+1)
+    k=int(input())
+
+    dp=[0]*(k+1)
     dp[0]=1
-    dp[coins[0]]=1
-    for i in range(coins[0],coins[1]):
-        if i%coins[0]==0:
-            dp[i]=1
+    for coin in coins:
+        for i in range(coin,k+1):
+            dp[i]+=dp[i-coin]
+
+    print(dp[k])
