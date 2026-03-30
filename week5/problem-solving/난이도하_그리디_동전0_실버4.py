f025cc1f66aad4b8403e4d23a8fca1fa4490ea8f
이재혁 <cncnvmvm@naver.com>
2026-03-30 10:44:48 +0900

    백준11047 그리디 동전

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,14 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+n,k=map(int,input().split())
+
+coins=[int(input()) for _ in range(n)]
+coins.reverse()
+count=0
+for coin in coins:
+    if coin>k:
+        continue
+    else:
+        count+=k//coin
+        k%=coin
+print(count)
